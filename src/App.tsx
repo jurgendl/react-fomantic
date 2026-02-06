4e943e0db8908f9122186b65e2320383ed14ed1f
jurgendl <jurgen.de.landsheer@gmail.com>
2026-02-06 22:37:51 +0100

    vite react fomantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import './App.scss'
+import * as React from 'react';
+import {type Dispatch, type SetStateAction, useEffect, useRef, useState} from 'react';
+import './App.scss';
+import styles from './App.module.scss';
 import {CheckboxDemoAlt} from "./CheckboxDemoAlt.tsx";
 
 function App() {
